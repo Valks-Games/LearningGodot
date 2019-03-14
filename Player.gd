d9c14158036fa0375de9edee2874222e6e2dba7d
valkyrienyanko <sebastianbelle074@gmail.com>
2019-03-13 19:11:03 -0500

    Player Movement

---
+++ Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+
+const SPEED = 100
+const MAX_SPEED = 200
+const ACCELERATION = 3
+const FRICTION = 0.1
+
+func _physics_process(delta):
+	if Input.is_action_pressed('ui_right'):
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+	elif Input.is_action_pressed('ui_left'):
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+	else:
+		motion.x = lerp(motion.x, 0, FRICTION)
+		
+	if Input.is_action_pressed('ui_down'):
+		motion.y = min(motion.y + ACCELERATION, MAX_SPEED)
+	elif Input.is_action_pressed('ui_up'):
+		motion.y = max(motion.y - ACCELERATION, -MAX_SPEED)
+	else:
+		motion.y = lerp(motion.y, 0, FRICTION)
+	
+	motion = move_and_slide(motion)
